bb9ff1e437031fc743b687e35435d41da338919a
10hesen044 <tu201906250@code.edu.az>
2021-10-25 08:25:52 +0400

    project added

---
+++ homework_oct24_tap_3/homework_oct24_tap_3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace homework_oct24_tap_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a;
+            a = 20;
+            Console.WriteLine(Math.Sqrt(a));
+            Console.ReadKey();        }
+    }
+}
